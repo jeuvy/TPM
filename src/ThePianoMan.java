005c0d54572c89b5397e8db0466c055b1db46051
jeuvy <jeuvalynegrace@gmail.com>
2016-05-02 13:03:32 -0700

    implemented Susan's

--- src/ThePianoMan.java
+++ src/ThePianoMan.java
@@ -21,48 +21,79 @@ public void setup()
 public void draw()
 {
   //declaring key variables 
+  int keyNum = 8;
   int wWidth = width/8;
   int wHeight = height-50;
   float bHeight = (3*height/5)-50;
   float bWidth = wWidth/2;
   int blackHover = -1;
   strokeWeight(2);
-  //println(blackHover);
+  
   
   //loop for white keys
   for (int wKey=0; wKey < width/wWidth; wKey++)
   {
     stroke(1);
-    float keyWidth = wKey * wWidth;
+    float wKeyLeft = wKey * wWidth;
+    float wKeyRight = wKeyLeft + wWidth;
+    float bKeyLeft = wKeyRight - bWidth/2;
+    float bKeyRight = bKeyLeft + bWidth;
+    
+ 
+   
+   if (wKey == 2 || wKey == 6 || wKey ==7){
+       continue;
+   }
    
     //white keys change color with mouse click
-    if(mouseX > keyWidth && mouseX <= keyWidth + wWidth && mouseY <= wHeight
-      && mousePressed && blackHover <= wKey+1
-      && get(mouseX,mouseY) != color(100, 100, 240)
-      && get(mouseX,mouseY) != color(105, 105, 130))
+    if(mouseX > bKeyLeft && mouseX <= bKeyRight && mouseY > 0
+      && mouseY <= bHeight){
+          blackHover = wKey;
+      }
      {
-       fill(100,100,100);
+    println(blackHover);
+       
      }
     
     //white keys change color with mouse hovering
-    else if (mouseX > keyWidth && mouseX <= keyWidth + wWidth && blackHover == -1
-            && get(mouseX,mouseY) != color(100, 100, 240)
-            && get(mouseX,mouseY) != color(105, 105, 130))
-     {
-        fill(200,100,100);
-     }
-     
-     //otherwise, keep the keys white
-     else
-     {
-       fill(255);
-     }
-    rect(keyWidth, 0, wWidth, height);
+   for (int i = 0; i < keyNum; i++){
+        float wKeyLeft = wKey * wWidth;
+        float wKeyRight = wKeyLeft + wWidth;
+        float bKeyLeft = wKeyRight - bWidth/2;
+        
+        if (mousePressed && mouseX >= wKeyLeft && mouseX <= wKeyRight && blackHover == -1){
+            fill(#F6B6B6);
+        }
+        else{
+            fill(255);
+        }
+        rect(keyWidth, 0, wWidth, height);
+   }
   }
  
   //draws the black keys
   for (int bKey=0; bKey<6; bKey++)
   {
+     int whiteKeyLeft = i * whiteKeyWidth; 
+     int whiteKeyRight = whiteKeyLeft + whiteKeyWidth;
+     int blackKeyLeft = whiteKeyRight - blackKeyWidth/2;
+     if (mousePressed && i == blackHover){
+         fill (#81FCDD);
+     }
+     else if (i == blackHover){
+         fill (#5A5050);
+     }
+     else{
+         fill (0);
+     }
+     
+     if (i != 2 && i != 6 && i !=7){
+         rect (bKeyLeft , 0, bWidth, bHeight);
+     }}}
+     }
+     }
+     }
+     }
     
     noStroke();
     float keyHeight= wWidth*(bKey+1)- bWidth/2;
@@ -101,8 +132,8 @@ public void draw()
 public void mousePressed()
 {
   int wWidth = width/8;
-  int wHeight = height-50;
-  float bHeight = (3*height/5)-50;
+  int wHeight = height;
+  float bHeight = 3*height/5;
   float bWidth = wWidth/2;
  
   for (int wKey=0; wKey < width/wWidth; wKey++)
